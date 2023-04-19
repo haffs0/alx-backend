489fc99697fb597f6adc98926d5b8734526e3ae8
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-04-19 07:57:26 +0000

    cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,12 +18,13 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
             self.cache_data.pop(first_key)
-            print("DISCARD: {}\n".format(first_key))
+            print("DISCARD:", first_key)
 
     def get(self, key):
         """ Get an item by key
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -18,11 +18,12 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if key is None or item is None:
+            return
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
             last_item = self.cache_data.popitem()
-            print("DISCARD: {}\n".format(last_item[0]))
+            print("DISCARD:", last_item[0])
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
